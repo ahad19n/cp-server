cfc07a94309fe9113e7b2e12bb885274971890c5
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2026-02-05 17:43:27 +0500

    fix typo

--- src/routes/Shop.routes.js
+++ src/routes/Shop.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllShops } = require('../controllers/Shop.model');
+const { getAllShops } = require('../controllers/Shop.controller');
 
 // -------------------------------------------------------------------------- //
 
