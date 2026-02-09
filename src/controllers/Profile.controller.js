e6475353502f19281ee5475045e9eef9731de6f8
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2026-02-09 23:45:16 +0500

    update profile controller

--- src/controllers/Profile.controller.js
+++ src/controllers/Profile.controller.js
@@ -1,19 +1,18 @@
 const { resp } = require('../func');
-const User = require('../models/User.model')
-
-// -------------------------------------------------------------------------- //
+const User = require('../models/User.model');
 
 exports.fetchUserProfile = async (req, res) => {
-    const user = await User.findOne({ number: req.token.number });
+    const user = await User.findOne({ number: req.token.number }, { _id: 0 });
     return resp(res, 200, 'Fetched user profile successfully', { profile: user });
 };
 
 exports.updateUserProfile = async (req, res) => {
     const { name } = req.body || {};
+    if (!name) return resp(res, 400, 'Missing or empty fields (name)');
 
     const user = await User.findOne({ number: req.token.number });
     user.name = name;
     user.save();
-    
+
     return resp(res, 200, 'Updated user profile successfully', { profile: user });
 };
