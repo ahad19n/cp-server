e06e0df63fbc259d2f06a03482397cf494f24048
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2026-02-10 00:34:04 +0500

    add dummy implementation for files controller

--- src/controllers/Files.controller.js
+++ src/controllers/Files.controller.js
@@ -3,13 +3,13 @@ const path = require('path');
 const crypto = require('crypto');
 
 exports.downloadFile = async (req, res) => {
-
+  return resp(res, 501, 'Not Implemented Yet');
 };
 
 exports.checkIfFileExists = async (req, res) => {
-
+  return resp(res, 501, 'Not Implemented Yet');
 };
 
 exports.processFileUpload = async (req, res) => {
-
+  return resp(res, 501, 'Not Implemented Yet');
 };
