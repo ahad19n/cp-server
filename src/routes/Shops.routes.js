2479872a33982626d5f8f72d971d68a9bfd85645
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2026-02-09 22:34:43 +0500

    fix typo in require file path

--- src/routes/Shops.routes.js
+++ src/routes/Shops.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllShops } = require('../controllers/Shop.controller');
+const { getAllShops } = require('../controllers/Shops.controller');
 
 // -------------------------------------------------------------------------- //
 
