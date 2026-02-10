9cd0807ff3a81e327d3c4b702312853dff85efd7
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2026-02-10 21:53:06 +0500

    fix missing id

--- src/controllers/Profile.controller.js
+++ src/controllers/Profile.controller.js
@@ -2,7 +2,7 @@ const { resp } = require('../func');
 const User = require('../models/User.model');
 
 exports.fetchUserProfile = async (req, res) => {
-  const user = await User.findOne({ number: req.token.number }, { _id: 0 });
+  const user = await User.findOne({ number: req.token.number });
   return resp(res, 200, 'Fetched user profile successfully', { profile: user });
 };
 
@@ -10,7 +10,7 @@ exports.updateUserProfile = async (req, res) => {
   const { name } = req.body || {};
   if (!name) return resp(res, 400, 'Missing or empty fields (name)');
 
-  const user = await User.findOne({ number: req.token.number }, { _id: 0 });
+  const user = await User.findOne({ number: req.token.number });
   user.name = name;
   await user.save();
 
